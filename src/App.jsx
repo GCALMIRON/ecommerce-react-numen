59ab0bf4aa094522350e67bfcca4e376927216cf
Emilio Carreira Neto <emi_carreira_neto@hotmail.com>
2023-09-14 17:00:25 -0300

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './assets/components/Navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+    <div className='container'>
+      <Navbar />
+    </div>
     </>
   )
 }
